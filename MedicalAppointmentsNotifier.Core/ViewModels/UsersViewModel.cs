02d07857d2680c3489ea549308dac18211f1defc
Dan Andreiu <andreiu_dan@yahoo.com>
2025-09-23 21:44:56 +0300

    added view model for users page

---
+++ MedicalAppointmentsNotifier.Core/ViewModels/UsersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MedicalAppointmentsNotifier.Domain.Entities;
+using MedicalAppointmentsNotifier.Domain.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace MedicalAppointmentsNotifier.Core.ViewModels
+{
+    public partial class UsersViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<User> users = new();
+
+        private IRepository<User> usersRepository;
+
+        public UsersViewModel(IRepository<User> repository)
+        {
+            usersRepository = repository ?? throw new ArgumentNullException(nameof(repository));
+            GetAll();
+        }
+
+        private void GetAll()
+        {
+            users = new ObservableCollection<User>(usersRepository.GetAll());
+        }
+    }
+}
