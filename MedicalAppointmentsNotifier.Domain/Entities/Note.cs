93a51b4409f1efaa41ccb3fc906ec9675c4de08c
Dan Andreiu <andreiu_dan@yahoo.com>
2025-09-23 18:03:03 +0300

    database creation
    created the database using entity framework code first and sqlite, also created the domain and data projects for better testing and separation of concerns

---
+++ MedicalAppointmentsNotifier.Domain/Entities/Note.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalAppointmentsNotifier.Domain.Entities
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public User? User { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null)
+            {
+                return false;
+            }
+
+            if(!(obj is Note))
+            {
+                return false;
+            }
+
+            return Equals(obj as Note);
+        }
+
+        private bool Equals(Note obj)
+        {
+            return this.Id.CompareTo(obj.Id) == 0 &&
+                this.Description == obj.Description &&
+                this.User.Equals(obj.User);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Description, User);
+        }
+    }
+}
---
+++ MedicalAppointmentsNotifier.Domain/Entities/User.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalAppointmentsNotifier.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public List<Appointment> Appointments { get; set; } = new List<Appointment>();
+
+        public List<Note> Notes { get; set; } = new List<Note>();
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null)
+            {
+                return false;
+            }
+
+            if(!(obj is User))
+            {
+                return false;
+            }
+
+            return Equals(obj as User);
+        }
+
+        private bool Equals(User obj)
+        {
+            return this.Id.CompareTo(obj.Id) == 0 &&
+                this.Name == obj.Name &&
+                this.Appointments.Equals(obj.Appointments) &&
+                this.Notes.Equals(obj.Notes);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name, Appointments, Notes);
+        }
+    }
+}
