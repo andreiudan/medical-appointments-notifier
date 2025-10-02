e723c2c057e0b909d6505f531ad2d475a79132c1
Dan Andreiu <andreiu_dan@yahoo.com>
2025-10-02 14:33:18 +0300

    NUnit project added and NameNormalizer tests

---
+++ MedicalAppointmentsNotifier.UnitTests/Core/Services/EntityToModelMapperTests.cs
@@ -0,0 +1,16 @@
+﻿using MedicalAppointmentsNotifier.Core.Services;
+
+namespace MedicalAppointmentsNotifier.UnitTests.Core.Services
+{
+    [TestFixture]
+    public class EntityToModelMapperTests
+    {
+        private EntityToModelMapper mapper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mapper = new EntityToModelMapper();
+        }
+    }
+}
